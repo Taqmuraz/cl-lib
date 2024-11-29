de2482c2a3a03810ff04871410e81d373bae9352
lulook <lulook23@gmail.com>
2024-11-29 23:38:59 +0100

    tassoc, table, tstr, partial, ->, pairs

---
+++ cl-lib.lisp
@@ -0,0 +1,43 @@
+(ql:quickload "alexandria")
+
+
+(defmacro -> (v &rest es)
+  (reduce
+    (lambda (a b) (cons (first b) (cons a (rest b))))
+    (map 'list (lambda (e) (if (listp e) e (list e))) es)
+    :initial-value v
+  )
+)
+
+(defun pairs (kv)
+  (reverse (reduce
+    (lambda (ac v)
+      (-> ac first rest null
+        (if
+          (-> ac first first (list v) (cons (rest ac)))
+          (cons (list v) ac)
+        )
+      )
+    )
+    (rest kv)
+    :initial-value (-> kv first list list)
+  ))
+)
+
+(defun tassoc (m &rest kv)
+  (reduce
+    (lambda (n p) (setf (gethash (first p) n) (second p)) n)
+    (pairs kv)
+    :initial-value (if (null m) (make-hash-table) (alexandria:copy-hash-table m))
+  )
+)
+
+(defun table (&rest kv) (apply #'tassoc (cons () kv)))
+
+(defun partial (f &rest pre)
+  (lambda (&rest post) (apply f (append pre post)))
+)
+
+(defun tstr (m)
+  (alexandria:hash-table-alist m)
+)
